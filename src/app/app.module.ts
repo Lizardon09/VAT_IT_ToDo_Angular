144aee06a8e1bf6d837c72a39bad188526abdf27
Varun Maharaj <varunmaharaj031@gmail.com>
2021-09-05 21:40:07 +0200

    chore: layout setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ToDoComponent } from './to-do/to-do.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ToDoComponent
+    ToDoComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
